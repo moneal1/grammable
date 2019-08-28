e53230eff554b5f5839c2a19388b7e1df5889f4d
Mike O'Neal <MikeONeal18@gmail.com>
2019-08-28 04:20:38 +0000

    lesson 7 test

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -10,4 +10,13 @@ RSpec.describe GramsController, type: :controller do
     
     end
 
+    describe "grams#new action" do
+        it "should successfully show the new form" do
+            get :new
+            expect(response).to have_http_status(:success)
+        
+        end
+    
+    end
+
 end
