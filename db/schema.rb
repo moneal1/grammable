e96971b404592ccf27fcee098277f34326a0b312
Mike O'Neal <MikeONeal18@gmail.com>
2019-08-29 03:55:55 +0000

    lesson 10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_28_235152) do
+ActiveRecord::Schema.define(version: 2019_08_29_021725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2019_08_28_235152) do
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_grams_on_user_id"
   end
 
   create_table "models", force: :cascade do |t|
